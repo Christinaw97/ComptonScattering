600e715b10d587ce0dce1385bd502db0ab2a3360
Christina Wang <cwang5@caltech.edu>
2022-04-18 20:43:19 -0700

    added helper functions

--- run_experiment.py
+++ run_experiment.py
@@ -1,12 +1,15 @@
 import argparse
 import sys
+import os
 from scipy.optimize import fsolve
 import math
 import numpy as np
-sys.path.append('/home/lab-cpt03/LinearStageRemoteControl/utilities/')
+sys.path.append(os.getcwd() + '/utilities/')
 import motor_tools
-import configparser
+import helper
+from helper import *
 
+import configparser
 
 def linear_calibration_prompt():
     print('current position of linear stage: %s cm' %linear.CurrentPos)
@@ -38,31 +41,9 @@ def rotary_calibration_prompt(homing, position):
 
 
 
-def equations(p):
-    a,b,c = p
-    t1 = deg2radian(theta1)
-    t2 = deg2radian(theta2)
-
-    return ( np.sqrt(a**2+b**2-2*a*b*math.cos(t1))-stage_length/2.,\
-   	     np.sqrt(a**2+c**2-2*a*c*math.cos(t2))-stage_length,\
-   	     np.sqrt(b**2+c**2-2*b*c*math.cos(t2-t1))-stage_length/2.)
-def deg2radian(deg): return deg/180.*math.pi
-def radian2deg(rad): return rad/math.pi*180
-
-def shift_calc(t_angle):
-    # Since the zero-position is the left of the linear stage, the rotator must move counterclockwise
-    target_rad = deg2radian(t_angle)
-    theta1_rad = deg2radian(theta1)
-    theta2_rad = deg2radian(theta2)
-    a,b,c=  fsolve(equations, (15.,15., 15.))
-    theta0=math.asin(b*math.sin(theta1_rad)/stage_length*2.)
-    x = a*math.sin(target_rad)/math.sin(theta0+target_rad) #absolute distance for linear stage in cm
-    return x
-
 if __name__ == '__main__':
 
-
-    # parse arguments
+# parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("target_deg", type=float, help="target angle")
     args = parser.parse_args()
@@ -73,27 +54,36 @@ if __name__ == '__main__':
     # parse config file
     config = configparser.ConfigParser()
     config.read('data/config.ini')
+    source_energy = float(config['source']['energy'])
+
+    print('Target energy is '+ str(target_energy(source_energy, 20)) + 'deg')
+    print('Tag energy is '+ str(target_energy(source_energy, 20)) + 'deg')
+
+    # stage configuration
     linear_config = config['linear_30']
     rotary_config = config['rotary']
-
     stage_length = float(linear_config['stage_length'])
+
+    # previous calibration
     theta1 = float(config['angle']['theta1'])
     theta2 = float(config['angle']['theta2'])
     print('previously measured angle theta1 theta2 (deg): ', theta1, theta2)
 
-    # establish connection and initialization
+
+    # establish connection and initialization the MDrives
     linear, rotary = motor_tools.Connection(str(linear_config['sn']),str(rotary_config['sn'])).connect_to_ports()
     linear.main(acceleration = 51200, max_vel = 100000, init_vel = 200, cm_per_rev = float(linear_config['cm_per_rev']), isLinear = True)
     rotary.main(acceleration = 51200, max_vel = 100000, init_vel = 200, cm_per_rev = 360./float(rotary_config['gear_ratio']), isLinear = False) #degree per rev, 90 is gear ratio
     
-    #prompt for recalibration of zero position and realigning the source
+    # prompt for recalibration of zero position and realigning the source
     linear_calibration_prompt()
     rotary_calibration_prompt(homing = True, position = 0)
     
-    #measure angle at mid point
+    # prompt to measure angle at mid point
     theta1_temp = rotary_calibration_prompt(homing = False, position = stage_length/2.0)
     if not theta1_temp == None: theta1 = abs(theta1_temp)
-    #measure angle at the farthest edge
+
+    # prompt measure angle at the farthest edge
     theta2_temp = rotary_calibration_prompt(homing = False, position = stage_length)
     if not theta2_temp == None:  theta2 = abs(theta2_temp)
     
@@ -101,7 +91,7 @@ if __name__ == '__main__':
     config.set('angle', 'theta1',str(theta1))
     config.set('angle', 'theta2',str(theta2))
 
-    # start taking data at target angle
+    # move to target position and start taking data at target angle
     lin_move = shift_calc(target_deg)
     print("moving to target angle "+ str(target_deg)+" deg and position " + str(round(lin_move,2))+" cm")
     linear.move_absolute(lin_move)
---
+++ utilities/helper.py
@@ -0,0 +1,40 @@
+# plot to calculate target/tag energy for certain incoming photon energy/angle
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+#angle conversions
+def deg2radian(deg): return deg/180.*math.pi
+def radian2deg(rad): return rad/math.pi*180
+
+#calculate expected energy at target/tag detector given incoming photon energy and angle
+def tag_energy(photon_in, theta):
+''' incoming photon energy in keV, theta in deg '''
+
+    return photon_in/(1+photon_in/511.0*(1-math.cos(deg2radian(theta))))
+def target_energy(photon_in, theta):
+	return photon_in-tag_energy(photon_in, theta)
+
+
+
+def equations(p):
+	''' used in function shift_calc to solve for a, b, c '''
+    a,b,c = p
+    t1 = deg2radian(theta1)
+    t2 = deg2radian(theta2)
+
+    return ( np.sqrt(a**2+b**2-2*a*b*math.cos(t1))-stage_length/2.,\
+   	     np.sqrt(a**2+c**2-2*a*c*math.cos(t2))-stage_length,\
+   	     np.sqrt(b**2+c**2-2*b*c*math.cos(t2-t1))-stage_length/2.)
+
+
+def shift_calc(t_angle):
+	''' given target angle, calculate the distance for linear stage to move in cm
+	t_angle is in unit of degree '''
+    target_rad = deg2radian(t_angle)
+    theta1_rad = deg2radian(theta1)
+    theta2_rad = deg2radian(theta2)
+    a,b,c=  fsolve(equations, (15.,15., 15.))
+    theta0=math.asin(b*math.sin(theta1_rad)/stage_length*2.)
+    x = a*math.sin(target_rad)/math.sin(theta0+target_rad) #absolute distance for linear stage in cm
+    return x
